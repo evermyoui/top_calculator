8b4a83fdfc23fa0bea0754a4538c43f6e99f472e
said <sexbythewind@gmail.com>
2025-10-29 18:42:27 +0800

    Started js functions and calculator class

--- script.js
+++ script.js
@@ -1,87 +1,49 @@
-let x = 0;
-let y = 0;
-let operator = null;
-let displayValues = "";
+// import doc elements
+const prevOperand = document.querySelector(".prev-operand");
+const currOperand = document.querySelector(".curr-operand");
+const numbers = document.querySelectorAll("[data-number]");
+const operations = document.querySelectorAll("[data-operation]");
+const allClear = document.querySelector("#all-clear");
+const deleteBtn = document.querySelector(".delete-btn");
+const equalBtn = document.querySelector("#equal-btn");
 
-const container = document.querySelector(".container");
-const display = document.querySelector(".display");
-
-const buttons = document.querySelectorAll('.buttons');
-const equalBtn = document.querySelector(".equal");
-const clearBtn = document.querySelector(".clear");
-const operatorBtn = document.querySelectorAll(".operator");
-
-buttons.forEach(button => {
-    button.addEventListener('click', ()=>{
-        const digit = button.textContent;
-        displayValues += digit;
-        display.textContent = displayValues;
-    });
-})
-
-operatorBtn.forEach(button =>{
-    button.addEventListener('click', ()=>{
-        if (!displayValues && operator){
-            operator = button.textContent;
-            return;
-        }
-        if (x && operator && displayValues){
-            y = displayValues;
-            const result = operate(operator, x, y);
-            x = result;
-            display.textContent = result;
-            displayValues = '';
-        }
-        else {
-            x = displayValues;
-            displayValues = '';
-        }
-        operator = button.textContent;
-    })
-})
-
-equalBtn.addEventListener('click', ()=>{
-    if (x && operator && displayValues){
-            y = displayValues;
-            const result = operate(operator, x, y);
-            x = result;
-            display.textContent = result;
-            displayValues = '';
-            operator = null;
-        }
-});
-
-clearBtn.addEventListener('click', ()=>{
-    x = 0;
-    y = 0;
-    operator = null;
-    displayValues = "";
-    display.textContent = "";
-});
-
-function operate(operator,x,y){
-    x = Number(x);
-    y = Number(y);
-    if (operator ==="+"){
-        return add(x,y);
-    }else if (operator === "-"){
-        return subtract(x,y);
-    }else if (operator === "*"){
-        return multiply(x,y);
-    }else if (operator === "/"){
-        return divide(x,y);
+class Calculator {
+    constructor(prevOperand, currOperand){
+        this.prevOperand = prevOperand;
+        this.currOperand = currOperand;
+        this.operation = undefined;
+        this.clear();
+    }
+    clear(){
+        this.currValue = "";
+        this.prevValue = "";
+    }
+    appendNumber(number){
+        if (number === "." && this.currValue.includes(".")) return;
+        this.currValue = this.currValue.toString() + number.toString();
+    }
+    displayCalc(){
+        currOperand.textContent = this.currValue;
+    }
+    chooseOperation(operation){
+        if (this.currValue === "") return;
+        this.operation = operation;
+        prevOperand.textContent = this.currValue;
+        this.currValue = "";
     }
 }
-function add(x,y){
-    return x + y;
-}
-function subtract(x,y){
-    return x - y;
-}
-function multiply(x,y){
-    return x * y;
-}
-function divide(x,y){
-    if (y ===0) return "ERROR";
-    return x / y;
-}
+
+const calculator = new Calculator(prevOperand, currOperand);
+
+numbers.forEach(number => {
+    number.addEventListener("click", () => {
+        calculator.appendNumber(number.textContent);
+        calculator.displayCalc();
+    })
+});
+operations.forEach(operation => {
+    operation.addEventListener("click", ()=> {
+        calculator.chooseOperation(operation);
+        calculator.displayCalc();
+    })
+})
